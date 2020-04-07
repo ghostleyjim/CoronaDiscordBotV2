ac140a02215387437cd7ac586cf085c3b6050a66
jim de groot <jimdegroot@gmail.com>
2020-04-07 19:48:11 +0200

    Initial commit

---
+++ Corona.py
@@ -0,0 +1,32 @@
+def dataextract():
+    with open('rivm_NL_covid19_hosp_municipality.csv', 'r') as database:
+
+        seperate = (line for line in database)
+        hospitalized_municipality = {}
+        hospitalized_province = {}
+
+        for x in seperate:
+            result = x.lower()
+            info = result.split(',')
+
+            date = info[ 0 ]
+            year_month_day = date.split('-')
+            month = year_month_day[ 1 ]
+            day = year_month_day[ 2 ]
+
+            municipality = info[ 1 ]
+            province = info[3]
+            cases = int((info[ 4 ].lower()).strip('\n'))
+
+            hospitalized_municipality.setdefault(municipality, []).append(cases)
+
+            # if [month, day]
+            # hospitalized_province.setdefault(province, []).append(cases)
+
+
+
+
+        # municipality = 'delft'  #  # if municipality in hospitalized:  #     print(hospitalized[municipality])  # else:  #     print('not found')
+
+
+dataextract()
---
+++ bot_new.py
@@ -0,0 +1,42 @@
+#!usr/bin/env python3
+
+import discord
+from dotenv import load_dotenv
+import os
+import Corona
+
+trigger = '!corona'
+
+load_dotenv()  # load the secret from the .env file
+TOKEN = os.getenv('DISCORD_TOKEN')  # variable token stores the secret
+PASTEAPI = os.getenv('PASTEBIN_TOKEN')
+PASTEUSERKEY = os.getenv('PASTEBIN_USERKEY')
+client = discord.Client()  # variable to store client info
+
+@client.event
+async def on_ready():  # if script connects to Discord
+    print(f'{client.user.name} has connected to Discord!')  # show I am connected with username
+
+
+@client.event
+async def on_message(message):  # if I reveive a message
+    incomming = message.content
+
+    if incomming.startswith("<"):
+        split_msg = incomming.split(' ')
+        if split_msg[1] == trigger:
+            del(split_msg[0:2])
+            #print(split_msg)
+
+    elif incomming.startswith(trigger):
+        split_msg = incomming.split(' ')
+        del(split_msg[0])
+        print(split_msg)
+        x = find.input(split_msg)
+
+
+
+
+
+
+client.run(TOKEN)  # run the client and login with secret
