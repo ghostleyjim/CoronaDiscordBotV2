a3321e97e5da1b7949206313a41aef5c52a444ae
jim de groot <jimdegroot@gmail.com>
2020-04-11 19:58:03 +0200

    code aangepast zodat slackbridge werkt

--- bot_new.py
+++ bot_new.py
@@ -19,24 +19,49 @@ def inputparser(split_msg):
     request_value = 0
     msglength = len(split_msg)
     municipality_value = [ ]
-    for x in range(msglength):
-        if split_msg[ x ].startswith("-"):
-            if split_msg[ x ][ 1: ].isdigit():
-                request_value = split_msg[ x ][ 1: ]
-        elif split_msg[ x ].isdigit():
-            request_value = split_msg[ x ]
 
-        else:
-            municipality_value.append(split_msg[ x ])
+    if split_msg[ 0 ] == 'help':
+        helpmsg = (f"Hello, I'm your personal Covid-19 assistent, here is what I can do for you\n"
+                   f"'!corona graph' - graphs with national information from the Netherlands\n"
+                   f"'!corona help' - shows you this page again\n"
+                   f"'!corona <municipality>' - information from the requested municipality\n"
+                   f"'!corona <municipality> <number>' - historical data from <number> days ago\n"
+                   f"'!corona listProv' - gives a list with all Provinces\n"
+                   f"'!corona listMun' - gives a list with all municipalities")
+        return helpmsg
 
-    length = len(municipality_value)
-    if length > 0:
-        location = ' '.join(municipality_value).lower()
-        send_ready = scraper.returnmunicipality(location, request_value)
-        return send_ready
+    elif split_msg[ 0 ] == "listmun":
+        listmunmsg = (f"The list of all municipalities is too long to print here.\n"
+                      f"check https://www.cbs.nl/-/media/_excel/2020/03/gemeenten-alfabetisch-2020.xlsx for more information")
+        return listmunmsg
+
+    elif split_msg[ 0 ] == "listprov":
+        provmsg = (f"Sorry, my devs are working on this at the moment")
+        try:
+            provmsg = scraper.listProv()
+        except:
+            pass
+        return provmsg
+
     else:
-        send_ready = "Error, municipality is unknown"
-        return send_ready
+        for x in range(msglength):
+            if split_msg[ x ].startswith("-"):
+                if split_msg[ x ][ 1: ].isdigit():
+                    request_value = split_msg[ x ][ 1: ]
+            elif split_msg[ x ].isdigit():
+                request_value = split_msg[ x ]
+
+            else:
+                municipality_value.append(split_msg[ x ])
+
+        length = len(municipality_value)
+        if length > 0:
+            location = ' '.join(municipality_value).lower()
+            send_ready = scraper.returnmunicipality(location, request_value)
+            return send_ready
+        else:
+            send_ready = "Error, municipality is unknown"
+            return send_ready
 
 
 @client.event
@@ -49,8 +74,24 @@ async def on_message(message):  # if I reveive a message
     incomming = message.content
 
     if incomming.startswith("<"):
-        errormsg = "Doe lekker normaal en voer gewoon een gemeente in..."
-        await message.channel.send(errormsg)
+        split_msg = incomming.split(' ')
+        if split_msg[ 1 ] == trigger:
+            del (split_msg[ 0:2 ])
+            if split_msg[ 0 ] == "graph":
+                try:
+                    graphs = plotNiceValues.createGraphs()
+                    graph1 = graphs[ 1 ]
+                    graph2 = graphs[ 2 ]
+
+                    my_files = [ discord.File(graph1, 'graph1.png'), discord.File(graph2, 'graph2.png') ]
+
+                    await message.channel.send('Graphs created by Diver', files=my_files)
+                except:
+                    errormsg = "Error in creating graphs"
+                    await message.channel.send(errormsg)
+            else:
+                send_ready = inputparser(split_msg)
+                await message.channel.send(send_ready)
 
     elif incomming.startswith(trigger):
         split_msg = incomming.split(' ')
@@ -69,29 +110,6 @@ async def on_message(message):  # if I reveive a message
             except:
                 errormsg = "Error in creating graphs"
                 await message.channel.send(errormsg)
-
-        elif split_msg[ 0 ].lower() == "help":
-            msg = (f"Hello, I'm your personal Covid-19 assistent, here is what I can do for you\n"
-                   f"'!corona graph' - graphs with national information fron the Netherlands\n"
-                   f"'!corona help' - shows you this page again\n"
-                   f"'!corona <municipality>' - information from the requested municipality\n"
-                   f"'!corona <municipality> <number>' - historical data from <number> days ago\n"
-                   f"'!corona listProv' - gives a list with all Provinces\n"
-                   f"'!corona listMun' - gives a list with all municipalities")
-            await message.channel.send(msg)
-
-        elif split_msg[ 0 ].lower() == "listprov":
-            msg = (f"Sorry, my devs are working on this at the moment")
-            try:
-                msg = scraper.listProv()
-            except:
-                pass
-            await message.channel.send(msg)
-
-        elif split_msg[ 0 ].lower() == "listmun":
-            msg = (f"The list of all municipalities is too long to print here.\n"
-                   f"check https://www.cbs.nl/-/media/_excel/2020/03/gemeenten-alfabetisch-2020.xlsx for more information")
-            await message.channel.send(msg)
 
         else:
             send_ready = inputparser(split_msg)
